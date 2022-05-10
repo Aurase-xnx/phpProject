9703d7866df4a1f7bdc5ca5497d1726746ec3913
Aurase-xnx <aurasebox@gmail.com>
2022-05-10 16:00:58 +0200

    update

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+date_default_timezone_set('Europe/Brussels');
+$host='localhost';
+$dbName='samplitek';
+$user='root';
+$mdp='';
+try {
+ $bd=new PDO('mysql:host='.$host.';dbname='.$dbName, $user, $mdp);
+ $bd->exec("SET NAMES 'utf8'");
+}
+catch (Exception $e) {
+ echo 'Error connecting to DB';
+}
