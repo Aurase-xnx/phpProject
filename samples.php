294279e4bc5ca1ad560d3c24e0169f7e553f957e
Aurase-xnx <aurasebox@gmail.com>
2022-05-12 14:05:28 +0200

    Final Update, not complete

--- samples.php
+++ samples.php
@@ -1,5 +1,127 @@
 <?php
   include_once('header.php');
   include_once('footer.php');
+  include_once('db.php');
+  if(isset($_SESSION['id']))
+  {
+    $creatorID = $_SESSION['id'];
+  }
+  $sampleName = $genre = $instrument = $bpm = "";
+  if (!isset($_POST['addSample']) ) {
+   //un champ obligatoire
+   if ( !empty($_POST['sampleName']) )
+   {
+      $sampleName = trim($_POST['sampleName']) ;
+   }
+   else
+   {
+       $mistakes['sampleName'] = true;
+   }
+
+   if ( !empty($_POST['genre'])){
+       $genre = trim($_POST['genre']) ;
+   }
+   else
+   {
+       $mistakes['genre'] = true;
+   }
+
+   if ( !empty($_POST['instrument'])){
+       $instrument = trim($_POST['instrument']) ;
+   }
+   else
+   {
+       $mistakes['instrument'] = true;
+   }
+
+   if ( !empty($_POST['bpm'])){
+       $bpm = trim($_POST['bpm']) ;
+   }
+   else
+   {
+       $mistakes['bpm'] = true;
+   }
+
+
+
+
+   //un champ obligatoire avec certaines valeurs rejetées
+
+
+
+
+   //s'il n'y a pas d'erreur...
+   if (empty($mistakes))
+   {
+       include("db.php");
+
+      $req=$bd->prepare('INSERT INTO  samples (sampleName,genre,instrument,bpm,creatorID) VALUES (:sampleName,:genre,:instrument,:bpm,:creatorID)');
+      $req->bindValue(':sampleName', $sampleName, PDO::PARAM_STR);
+      $req->bindValue(':genre', $genre, PDO::PARAM_STR);
+      $req->bindValue(':instrument', $instrument, PDO::PARAM_STR);
+      $req->bindValue(':bpm', $bpm, PDO::PARAM_STR);
+      $req->bindValue(':creatorID', $creatorID, PDO::PARAM_STR);
+      echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
+      $req->execute();
+      $req->closeCursor();
+      header("Location:samples.php");
+      exit();
+
+   }
+   else{
+     print_r($mistakes);
+   }
+  }
+
+  $host = 'localhost';
+   $dbname = 'samplitek';
+   $usern = 'root';
+   $passw = '';
+  $dsn = "mysql:host=$host;dbname=$dbname";
+  // get all users
+  $sql = "SELECT * FROM samples";
+
+  try{
+   $pdo = new PDO($dsn, $usern, $passw);
+   $stmt = $pdo->query($sql);
+
+   if($stmt === false){
+    die("Error");
+   }
+
+  }catch (PDOException $e){
+    echo $e->getMessage();
+  }
+
  ?>
 <p>Samples Here</p>
+<form action="samples.php" method="post">
+  <input type="text" name="sampleName" id="sampleName" required="" placeholder="Sample Name">
+  <input type="text" name="genre" id="genre" required="" placeholder="Genre">
+  <input type="text" name="instrument" id="instrument" required="" placeholder="Instrument">
+  <input type="text" name="bpm" id="name" required="" placeholder="BPM">
+  <input type="submit" name="addSample" placeholder="Upload the sample">
+</form>
+
+<table>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Sample Name</th>
+      <th>Genre</th>
+      <th>Instrument</th>
+      <th>BPM</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+    <tr>
+      <td><?php echo htmlspecialchars($row['id']); ?></td>
+      <td><?php echo htmlspecialchars($row['sampleName']); ?></td>
+      <td><?php echo htmlspecialchars($row['genre']); ?></td>
+      <td><?php echo htmlspecialchars($row['instrument']); ?></td>
+      <td><?php echo htmlspecialchars($row['bpm']); ?></td>
+    </tr>
+    <?php endwhile; ?>
+  </tbody>
+</table>
--- songs.php
+++ songs.php
@@ -1,5 +1,106 @@
 <?php
   include_once('header.php');
   include_once('footer.php');
+  include_once('db.php');
+  if(isset($_SESSION['id']))
+  {
+    $creatorID = $_SESSION['id'];
+  }
+  $songName = $genreS = $bpm = "";
+  if (!isset($_POST['addSong']) ) {
+   //un champ obligatoire
+   if ( !empty($_POST['songName']) )
+   {
+      $songName = trim($_POST['songName']) ;
+   }
+   else
+   {
+       $mistakes['songName'] = true;
+   }
+
+   if ( !empty($_POST['genreS'])){
+       $genreSS = trim($_POST['genreS']) ;
+   }
+   else
+   {
+       $mistakes['genreS'] = true;
+   }
+
+   if ( !empty($_POST['bpmS'])){
+       $bpmS = trim($_POST['bpmS']) ;
+   }
+   else
+   {
+       $mistakes['bpmS'] = true;
+   }
+
+   //s'il n'y a pas d'erreur...
+   if (empty($mistakes))
+   {
+       include("db.php");
+
+      $req=$bd->prepare('INSERT INTO  songs (songName,genre,bpm,creatorID) VALUES (:songName,:genre,:bpm,:creatorID)');
+      $req->bindParam(':songName', $songName, PDO::PARAM_STR);
+      $req->bindParam(':genre', $genreS, PDO::PARAM_STR);
+      $req->bindParam(':bpm', $bpmS, PDO::PARAM_STR);
+      $req->bindParam(':creatorID', $creatorID, PDO::PARAM_STR);
+      $req->execute();
+      $req->closeCursor();
+      header("Location:songs.php");
+      exit();
+
+   }
+   else{
+     print_r($mistakes);
+   }
+  }
+
+  $host = 'localhost';
+   $dbname = 'samplitek';
+   $usern = 'root';
+   $passw = '';
+  $dsn = "mysql:host=$host;dbname=$dbname";
+  // get all users
+  $sql = "SELECT * FROM songs";
+
+  try{
+   $pdo = new PDO($dsn, $usern, $passw);
+   $stmt = $pdo->query($sql);
+
+   if($stmt === false){
+    die("Error");
+   }
+
+  }catch (PDOException $e){
+    echo $e->getMessage();
+  }
+
  ?>
 <p>Songs Here</p>
+<form action="songs.php" method="post">
+  <input type="text" name="songName" id="songName" required="" placeholder="Song Name">
+  <input type="text" name="genreS" id="genreS" required="" placeholder="Genre">
+  <input type="text" name="bpms" id="bpmS" required="" placeholder="BPM">
+  <input type="submit" name="addSong" required="Upload the song ">
+</form>
+
+<table>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Song Name</th>
+      <th>Genre</th>
+      <th>BPM</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+    <tr>
+      <td><?php echo htmlspecialchars($row['id']); ?></td>
+      <td><?php echo htmlspecialchars($row['songName']); ?></td>
+      <td><?php echo htmlspecialchars($row['genre']); ?></td>
+      <td><?php echo htmlspecialchars($row['bpm']); ?></td>
+    </tr>
+    <?php endwhile; ?>
+  </tbody>
+</table>
